b00d8872a1f4f282a8de0e5fa9bc292187325566
Sebastian Gonzalez <go.sebastian12@gmail.com>
2020-11-30 19:14:29 -0600

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Tracking/predicting futbol formation changes and predicting substitutions with tracking data.',
+    author='Sebastian E. Gonzalez',
+    license='',
+)
