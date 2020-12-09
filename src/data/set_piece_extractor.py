39ce33444a695e7777b0f1e627a1fd06415df2bc
Sebastian Gonzalez <go.sebastian12@gmail.com>
2020-12-08 18:15:22 -0600

    Beginning set piece extraction.

---
+++ src/data/set_piece_extractor.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 30 7:20:02 2020
+
+@author: Sebastian E. Gonzalez
+
+Script Purpose: Write functions that allow the user to take the raw
+Wyscout event-log data and obtain a collection of all of the set pieces
+and their subsequent plays that are present.
+"""
+####################################
+### Neccessary Import Statements ###
+####################################
+# data manipulation
+import pandas as pd
+import numpy as np
+
+# custom modules
+
+# define variables that will be used throughout script
+SCRIPT_DIR = os.path.dirname(__file__)
+
+
+################################
+### Define Modular Functions ###
+################################
+def hi() -> :
+	"""
+	Purpose
+	-------
+	The purpose of this function is to
+
+	Parameters
+	----------
+	 : str
+		This argument allows the user to specify
+
+	Returns
+	-------
+	to_return : s
+		This function returns 
+
+	References
+	----------
+	1. 
+	"""
+	to_return = None
+	# First,
+
+	return to_return
+
+
+def hi() -> :
+	"""
+	Purpose
+	-------
+	The purpose of this function is to
+
+	Parameters
+	----------
+	 : str
+		This argument allows the user to specify
+
+	Returns
+	-------
+	to_return : s
+		This function returns 
+
+	References
+	----------
+	1. 
+	"""
+	to_return = None
+	# First,
+
+	return to_return
+
+
+def hi() -> :
+	"""
+	Purpose
+	-------
+	The purpose of this function is to
+
+	Parameters
+	----------
+	 : str
+		This argument allows the user to specify
+
+	Returns
+	-------
+	to_return : s
+		This function returns 
+
+	References
+	----------
+	1. 
+	"""
+	to_return = None
+	# First,
+
+	return to_return
